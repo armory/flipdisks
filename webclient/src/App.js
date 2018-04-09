896ca04ec9fd98fb9250d1d4d88a3849768d3404
jcheng305 <jccoding305@gmail.com>
2018-04-09 14:47:36 -0700

    Feature/web display (#2)
    * move payload out to json
    
    * displaying boards and fixing some data in payload
    
    * displaying boards and fixing some data in payload

--- webclient/src/App.js
+++ webclient/src/App.js
@@ -3,128 +3,58 @@ import logo from './logo.svg';
 import './App.css';
 import * as _ from 'lodash';
 
+
+
+const payload = require('./payload.json');
+
+
 class App extends Component {
 	constructor() {
 		super();
-
-		this.state = {
-			screen: [],
-			screens: [
-				[
-					[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-					[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-					[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-					[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-					[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-					[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-					[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-					[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-					[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-					[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-				],
-				[
-					[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-					[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-					[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-					[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-					[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-					[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-					[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-					[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-					[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-					[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-				]
-			]
-		};
-
-		this.count = 0;
-
-		this.res = {
-			refreshRate: 100,
-			screen: [
-				[
-					[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-					[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-					[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-					[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-					[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-					[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-					[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-					[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-					[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-					[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-				],
-			]
-		};
-
-		_.forEach(_.range(this.res.screen[0].length - 1), (index) => {
-			const newScreen = _.cloneDeep(this.res.screen[index]);
-			newScreen.unshift(newScreen.pop());
-			this.res.screen.push(newScreen);
-		});
-
-		this.screenTwoRes = {
-			refreshRate: 100,
-			screen: [
-				[
-					[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-					[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-					[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-					[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-					[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-					[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-					[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-					[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-					[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-					[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-				],
-			]
-		};
-
-		_.forEach(_.range(this.res.screen[0].length - 1), (index) => {
-			const newScreen = _.cloneDeep(this.screenTwoRes.screen[index]);
-			newScreen.unshift(newScreen.pop());
-			this.screenTwoRes.screen.push(newScreen);
-		});
-
 	}
 
-	componentWillMount() {
+
+	componentDidMount() {
+		let frameNumber = 0;
+
 		setInterval(() => {
-			this.count = (this.count + 1) % this.res.screen.length;
-			this.setState({screen: this.res.screen[this.count]});
+			const state = {
+				layout: payload.layout,
+			};
 
-			this.setState({
-				screens: [
-					this.res.screen[this.count],
-					this.screenTwoRes.screen[this.count],
-				]
+			_.flatten(payload.layout).forEach(boardName => {
+				state[boardName] = payload.frames[frameNumber][boardName];
 			});
-		}, this.res.refreshRate);
+
+			this.setState(state);
+
+			frameNumber = (frameNumber + 1) % payload.frames.length;
+		}, payload.frameRate);
 	}
+
 
 	render() {
+		if (!this.state) {
+			return (<div></div>)
+		}
+
 		return (
 			<div className="App">
 				<header className="App-header">
 					<img src={logo} className="App-logo" alt="logo"/>
 					<h1 className="App-title">Welcome to React</h1>
 				</header>
-				<pre>
-					{this.state.screen.map(screen => {return (<div>{screen.join(' ')}</div>)})}
-				</pre>
-				<div style={{
-					margin: 'auto',
-					width: '50%',
-				}}>
-					{this.state.screens.map(screen => {
-						return (<span>
-						{<pre style={{float: 'right', 'margin-left': '20px'}}>{screen.map(line => {
-							return (<div>{line.join(' ')}</div>)
-						})}</pre>}
-					</span>);
+				<div className="board-container">
+					{this.state.layout.map(yAxisBoardNames => {
+						return yAxisBoardNames.map(name => {
+							return (<div>{name}
+								{this.state[name].map(x => {
+									return (<div>{x.join(' ')}</div>);
+								})}</div>);
+						})
 					})}
 				</div>
+
 			</div>
 		);
 	}
