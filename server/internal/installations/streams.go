20e8841984e86e7363e08ebc48e91ae0eebbb3c6
jcheng305 <jccoding305@gmail.com>
2018-04-19 22:21:08 -0700

    Feature/web display (#6)
    * added some styling to page
    
    * defined the payload structure

--- server/internal/installations/streams.go
+++ server/internal/installations/streams.go
@@ -6,10 +6,17 @@ import (
 )
 
 
+type Row []int
+type Board []Row
+type Frame map[string]Board
+
+
 type streamResponse struct {
-	Installation string `json:"installation"`
-	FrameRate    int    `json:"frameRate"`
-	Looping      bool   `json:"looping"`
+	Installation string     `json:"installation"`
+	FrameRate    int        `json:"frameRate"`
+	Looping      bool       `json:"looping"`
+	Layout			 [][]string `json:"layout"`
+	Frames 			 []Frame    `json:"frames"`
 }
 
 
@@ -22,6 +29,20 @@ func GetStream(c *gin.Context) {
 		Installation: installationLocation,
 		FrameRate:    1000,
 		Looping:      true,
+		Layout:				[][]string {
+	    []string{"board1", "board2", "board3"},
+	    []string{"board4", "board5", "board6"},
+	    []string{"board7", "board8", "board9"},
+	    []string{"board7", "board8", "board9"},
+  	},
+		Frames: []Frame{
+			Frame{
+				"board1": Board{
+					Row{1, 1, 1, 1, 1, 1},
+					Row{1,1,1,1,1,1,1},
+				},
+			},
+		},
 	}
 
 	c.JSON(200, stream)
