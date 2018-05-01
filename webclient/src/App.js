4b5f1e6a403f68892fa14ccf3f93455c4df7741f
Jimmy Cheng <jccoding305@gmail.com>
2018-04-30 20:26:18 -0700

    stuff

--- webclient/src/App.js
+++ webclient/src/App.js
@@ -2,7 +2,9 @@ import React, { Component } from 'react';
 import './App.css';
 import * as _ from 'lodash';
 import * as request from 'request-promise';
-
+import { debug } from 'util';
+// import Font from '/Users/jcheng305/go/src/github.com/armory/flipdisks/webclient/src/Font.js';
+// import DropDown from '/Users/jcheng305/go/src/github.com/armory/flipdisks/webclient/src/DropDown.js';
 
 const config = {
 	server: {
@@ -17,30 +19,72 @@ const config = {
 
 
 class App extends Component {
-	constructor() {
-		super();
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			text: "Hello",
+			board: null
+		}
+
+		this.handleKeyPress = this.handleKeyPress.bind(this);
 	}
 
+
+
 	async componentDidMount() {
-		const board = await request.post({
+ 		const board = await request.post({
 			url: `${config.server.baseUrl}/v1/fonts/render`,
 			json: true,
 			body: {
 				"fontName": "TI84",
-				"text": "good job",
-				"spaceWidth": 4,
+				"text": "",
+				"spaceWidth": 0,
 				"kerning": 0
 			}
 		});
 
-		console.log(board)
 		this.setState({
+			// text: "Test",
 			board: board
 		});
+		console.log(board)
 	}
+
+	handleKeyPress(event) {
+		this.setState({
+			text: event.target.value
+		});
+		
+		return request.post({
+			url: `${config.server.baseUrl}/v1/fonts/render`,
+			json: true,
+			body: {
+				"fontName": "TI84",
+				"text": event.target.value,
+				"spaceWidth": 0,
+				"kerning": 0
+			}
+		})
+		.then((board) => {
+			this.setState({
+				board:board
+			})
+		})
+		
+		console.log(2)
+				// this.setState({
+		// 	board: board
+		// });
+		console.log(3)
+	}
+
 
 	render() {
 		const drawABoard = () => {
+			if (!this.state.board) {
+				return (<div></div>)
+			}
 			// for each row, create a span of columns
 			return (
 				<div className="board-container">
@@ -73,10 +117,18 @@ class App extends Component {
 				<header className="App-header">
 					<h1 className="App-title">Flip Disc Simulator</h1>
 				</header>
+
+				<div className="text-box-container">
+					Font Type<select></select>
+					Grid Size<select></select>
+					Font Size<select></select>
+				</div>
+				<input type="text" onChange={this.handleKeyPress} value={this.state.text}></input>
 				<div className="">
 					{drawABoard()}
 				</div>
-
+				{/* <Font/> */}
+				{/* <DropDown/> */}
 			</div>
 		);
 	}
