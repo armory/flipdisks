9381073d291e8bc98c1db3b3d92e253e66e3d745
Kevin Woo <kevinawoo@gmail.com>
2018-08-27 23:02:19 -0700

    stub(utils)

--- controller/pkg/image/image_test.go
+++ controller/pkg/image/image_test.go
@@ -1,4 +1,4 @@
-package flipimage
+package image
 
 import (
 	"image"
@@ -7,6 +7,7 @@ import (
 	"testing"
 
 	"github.com/armory/flipdisks/controller/pkg/fontmap"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestConvert(t *testing.T) {
@@ -49,5 +50,79 @@ func TestConvert(t *testing.T) {
 		t.Error("images are not equal")
 		t.Errorf("Got %v", v)
 		t.Errorf("Expected %v", armory)
+	}
+}
+
+func TestIsPlainImageUrl(t *testing.T) {
+	tests := map[string]struct {
+		url string
+
+		expected bool
+	}{
+		".png": {
+			url:      "http....",
+			expected: true,
+		},
+		".jpg": {
+			url:      "https://blah.jpg",
+			expected: true,
+		},
+		".jpeg": {
+			url:      "https://blah.jpeg",
+			expected: true,
+		},
+		".txt": {
+			url:      "http://blah.txt",
+			expected: true,
+		},
+		"no extension": {
+			url:      "http://google.com",
+			expected: true,
+		},
+	}
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			assert.True(t, IsPlainImageUrl(test.url) == test.expected, "failed message")
+
+		})
+	}
+}
+
+func TestIsGifUrl(t *testing.T) {
+	tests := map[string]struct {
+		url string
+
+		expected bool
+	}{
+		".gif": {
+			url:      "http....",
+			expected: true,
+		},
+		".png": {
+			url:      "http....",
+			expected: false,
+		},
+		".jpg": {
+			url:      "https://blah.jpg",
+			expected: false,
+		},
+		".jpeg": {
+			url:      "https://blah.jpeg",
+			expected: false,
+		},
+		".txt": {
+			url:      "http....",
+			expected: false,
+		},
+		"no extension": {
+			url:      "http....",
+			expected: false,
+		},
+	}
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			assert.True(t, IsGifUrl(test.url) == test.expected, "failed message")
+
+		})
 	}
 }
