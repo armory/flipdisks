69783fdc8263d59480f647007d7d0510f24203c7
Kevin Woo <kevinawoo@gmail.com>
2019-02-18 21:54:14 -0800

    feat(deploy): add port variable

--- bin/deploy-controller.sh
+++ bin/deploy-controller.sh
@@ -1,13 +1,14 @@
 #!/bin/bash -xe
 
 
-IP=${1:-"flipdisk.local"}
+HOST=${1:-"flipdisk.local"}
+PORT=${1:-"22"}
 
 # upload load files
-rsync -avzI controller/etc/flipdisk.service "pi@${IP}:/tmp/"
-rsync -avzI controller/build/main "pi@${IP}:/home/pi/Desktop/"
+rsync -avzI --port ${PORT} controller/etc/flipdisk.service "pi@${HOST}:/tmp/"
+rsync -avzI --port ${PORT} controller/build/main "pi@${HOST}:/home/pi/Desktop/"
 
-ssh pi@${IP} << EOF
+ssh -p ${PORT} pi@${HOST} << EOF
   sudo mv /tmp/flipdisk.service /lib/systemd/system/.
   sudo chmod 755 /lib/systemd/system/flipdisk.service
   sudo systemctl enable flipdisk.service
